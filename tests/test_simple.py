4580b9ea47d4e41f690406c0f54851c7862f4ab1
Kyle Lahnakoski <kyle@lahnakoski.com>
2021-08-28 11:27:45 -0400

    cleanup debuggers

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -8,10 +8,7 @@
 
 from __future__ import absolute_import, division, unicode_literals
 
-import json
 from unittest import TestCase
-
-from mo_parsing.debug import Debugger
 
 from mo_sql_parsing import parse
 
