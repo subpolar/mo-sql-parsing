158e6b633f40a9441a3a2755be756df5adba82c5
Kyle Lahnakoski <kyle@lahnakoski.com>
2021-09-19 11:19:06 -0400

    docs

--- tests/smoke_test.py
+++ tests/smoke_test.py
@@ -6,26 +6,11 @@
 #
 # Author: Kyle Lahnakoski (kyle@lahnakoski.com)
 #
-import json
-
-from mo_parsing.utils import listwrap
 
 from mo_sql_parsing import parse
+from mo_sql_parsing.utils import normal_op
 
-
-def normalize(expression):
-    if isinstance(expression, dict):
-        items = list(expression.items())
-        if len(items) == 1:
-            return [
-                {"operator": operator, "args": [normalize(p) for p in listwrap(args)]}
-                for operator, args in items
-            ][0]
-        else:
-            raise NotImplemented()
-    return expression
-
-
-result = parse("select myFunction(a, b+c)")
-print(json.dumps(normalize(result['select']['value'])))
+sql = "select trim(' ' from b+c)"
+result = parse(sql, calls=normal_op)
+print(result)
 print("done")
