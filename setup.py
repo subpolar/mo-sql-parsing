7f483a8ede5a3deb6d3f1f894cbd945b0886a099
Kyle Lahnakoski <kyle@lahnakoski.com>
2022-05-01 11:23:54 -0400

    update version number

--- setup.py
+++ setup.py
@@ -16,6 +16,6 @@ setup(
     name='mo-sql-parsing',
     packages=["mo_sql_parsing"],
     url='https://github.com/klahnakoski/mo-sql-parsing',
-    version='8.170.22121',
+    version='8.171.22121',
     zip_safe=True
 )
